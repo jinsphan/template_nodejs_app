3efda16adc39004c96537109126a63530a601272
jinsphan <pvtinh1996@gmail.com>
2018-02-23 12:33:20 +0700

    Fix checkTokenExpiry

--- src/app/middleware/checkTokenExpiry.js
+++ src/app/middleware/checkTokenExpiry.js
@@ -2,10 +2,8 @@ var checkTokenExpiry = (req, res, next) => {
   var expiry = req.userDecoded.exp;
   var expiried = (new Date().getTime()) - expiry;
   if (expiried > 0) {
-	  console.log("Het han", expiried);
-	  res.status(401).json({"error": "Token has expiried"});
+	  res.status(401).json({"error": "The token has expiried"});
   }
-  else console.log("Con han", expiried);
   next();
 }
 
