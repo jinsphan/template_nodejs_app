e230e54945354ff4e990930273d521bcadeccf83
= <=>
2018-02-23 11:26:23 +0700

    notify error when token has expiried

--- src/app/middleware/checkTokenExpiry.js
+++ src/app/middleware/checkTokenExpiry.js
@@ -1,7 +1,10 @@
 var checkTokenExpiry = (req, res, next) => {
   var expiry = req.userDecoded.exp;
   var expiried = (new Date().getTime()) - expiry;
-  if (expiried > 0) console.log("Het han", expiried);
+  if (expiried > 0) {
+	  console.log("Het han", expiried);
+	  res.status(401).json({"error": "Token has expiried"});
+  }
   else console.log("Con han", expiried);
   next();
 }
